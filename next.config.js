79655b3d2c27ca11dcfae7b9bd186f5a70e779af
MatheusMarques13 <135287990+MatheusMarques13@users.noreply.github.com>
2026-03-10 16:13:09 -0300

    fix: add next.config.js (compatible with Next.js 14.2)

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lastfm.freetls.fastly.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
